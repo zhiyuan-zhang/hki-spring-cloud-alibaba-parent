c346c444932eef737f01538bcebda40f1319a521
zhanghao <913314986@qq.com>
2019-12-02 17:22:13 +0800

    添加nacos config

--- hki-spring-cloud-alibaba-nacos-provider/src/main/java/hki/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hki-spring-cloud-alibaba-nacos-provider/src/main/java/hki/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,5 +1,8 @@
 package hki.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,30 @@ public class NacosProviderController {
     public String echo(@PathVariable String string){
         return "Hello Nacos Discovery " + string;
     }
+
+
+    // 注入配置文件上下文
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    // 从上下文中读取配置
+    @GetMapping(value = "/hi")
+    public String sayHi() {
+        // 实时获取刷新数据 保证拿到最新的
+        return "Hello " + applicationContext.getEnvironment().getProperty("user.name");
+    }
+
+
+    // 注入配置文件上下文
+    @Value("${user.name}")
+    private String name;
+
+    // 从上下文中读取配置
+    @GetMapping(value = "/value")
+    public String getName() {
+        // 这个会写入内存 启动程序后再去更改配置 并不会改变
+        return "Hello value = " + name;
+    }
+
 
 }
